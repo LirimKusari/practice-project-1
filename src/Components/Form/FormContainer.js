c15c324a4dcf20478d7e1aadffda63bad4d2d798
Lirim Kusari <kusarilirim@gmail.com>
2021-08-24 18:07:50 +0200

    throw away

--- src/Components/Form/FormContainer.js
+++ src/Components/Form/FormContainer.js
@@ -1,3 +1,16 @@
 const FormContainer = () => {
-  return <form action=""></form>;
+  const addUserHandler = () => {
+    console.log("hellooo");
+  };
+  return (
+    <form>
+      <label for="fname">First name:</label>
+      <input type="text" id="fname" name="fname" />
+      <label for="age">Last name:</label>
+      <input type="number" id="lname" name="lname" />
+      <button onSubmit={addUserHandler}></button>
+    </form>
+  );
 };
+
+export default FormContainer;
